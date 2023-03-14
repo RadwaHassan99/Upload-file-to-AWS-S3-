3015e15af07800ab6b6c92434cff90e7b782c679
Radwa Hassan <63107268+RadwaHassan99@users.noreply.github.com>
2023-03-14 21:58:11 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("_ACCESS_KEY_", 'AKIAURMNXEKZHGHK24VY');
-define("_SECRET_KEY_", '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');
-define("_BUCKET_NAME_", 'formmmm');
-define("_BUKET_REGION_","eu-central-1");
-?>
